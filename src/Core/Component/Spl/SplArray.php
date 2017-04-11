73f380957de3c4a67a371663b7b44f3351ca04dc
kiss291332003 <z15935700>
2017-04-11 15:04:31 +0800

    add splArray

---
+++ src/Core/Component/Spl/SplArray.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2017/4/11
+ * Time: 下午3:01
+ */
+
+namespace Core\Component\Spl;
+
+
+class SplArray extends \ArrayObject
+{
+    public function __construct(array $array = array())
+    {
+        parent::__construct($array);
+    }
+
+    function __get($name)
+    {
+        // TODO: Implement __get() method.
+        if(isset($this[$name])){
+            return $this[$name];
+        }else{
+            return null;
+        }
+    }
+
+    function __set($name, $value)
+    {
+        // TODO: Implement __set() method.
+        $this[$name] = $value;
+    }
+
+    function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return json_encode($this,JSON_UNESCAPED_UNICODE,JSON_UNESCAPED_SLASHES);
+    }
+}
