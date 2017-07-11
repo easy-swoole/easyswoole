3d62e3e1aa766786e6396a6f27945eb00fe92c27
kiss291332003 <z15935700>
2017-07-11 13:38:13 +0800

    update

--- example/multiUsage_01/App/Controller/Test/Udp.php
+++ example/multiUsage_01/App/Controller/Test/Udp.php
@@ -16,9 +16,6 @@ class Udp extends AbstractController
 {
     /*
      * /test/udp/multiProtocol/
-     * 当前有bug，在Server启动前监听udp，在Event下，相同client在cli模式下，可以获得服务端返回数据，但在
-     * swoole http server onrequest下失效
-     *  cli下udp客户端测试代码  echo \App\Model\UdpClient::sendTo("127.0.0.1",9502,'as');
      */
     function multiProtocol(){
         $data = UdpClient::sendTo('127.0.0.1','9502',"i am client");
