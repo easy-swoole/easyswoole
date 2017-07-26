3f30c3bed25b875cd8f7ee65279f60c846166075
kiss291332003 <z15935700>
2017-07-26 10:37:19 +0800

    mysqli断线重连

---
+++ example/mysqliReconnect/App/Utility/SysConst.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2017/7/26
+ * Time: 上午10:04
+ */
+
+namespace App\Utility;
+
+
+class SysConst extends \Core\Component\SysConst
+{
+    const MYSQL = 'MYSQL';
+}
