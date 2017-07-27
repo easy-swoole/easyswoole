ebfe9369bd89b261c3c69f86d21ec25390a3ce86
xuhaoxian <312263441@qq.com>
2017-07-27 16:46:47 +0800

    数据校验类支持students.*.name字段校验

--- src/Core/Utility/Validate/Validate.php
+++ src/Core/Utility/Validate/Validate.php
@@ -43,10 +43,27 @@ class Validate
                 unset($checkRuleMap['ruleMap']['OPTIONAL']);
             }
             foreach ($checkRuleMap['ruleMap'] as $rule => $ruleData){
-                if(!Func::$rule($column,$data,$ruleData['args'])){
-                    $msg = $ruleData['msg'] ? $ruleData['msg'] : $checkRuleMap['errorMsg'];
-                    $errorMessages[$column][] = $msg;
+                //change by xhx 2017年07月27日15:54:57
+                //取出来判断验证的值是不是数组 是的话循环验证
+                $values = $data->get($column);
+                if (is_array($values)){
+                    $splArr = new SplArray();
+                    foreach ($values as $value){
+                        $splArr->set('tempKey', $value);
+                        if(!Func::$rule('tempKey', $splArr,$ruleData['args'])){
+                            $msg = $ruleData['msg'] ? $ruleData['msg'] : $checkRuleMap['errorMsg'];
+                            $errorMessages[$column][] = $msg;
+                            //这个字段遇到错误就不检测了直接跳出
+                            break;
+                        }
+                    }
+                }else{
+                    if(!Func::$rule($column,$data,$ruleData['args'])){
+                        $msg = $ruleData['msg'] ? $ruleData['msg'] : $checkRuleMap['errorMsg'];
+                        $errorMessages[$column][] = $msg;
+                    }
                 }
+
             }
         }
         return new Message($errorMessages);
