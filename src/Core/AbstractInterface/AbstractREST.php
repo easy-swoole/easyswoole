152985e9163b9930e0f84a3db5a42beed9dd6336
kiss291323003 <291323003@qq.com>
2017-09-13 00:16:04 +0800

    新增RESTful支持

--- src/Core/AbstractInterface/AbstractREST.php
+++ src/Core/AbstractInterface/AbstractREST.php
@@ -11,6 +11,16 @@ namespace Core\AbstractInterface;
 
 abstract class AbstractREST extends AbstractController
 {
+    /*
+     * 支持方法
+         'GET',      // 从服务器取出资源（一项或多项）
+        'POST',     // 在服务器新建一个资源
+        'PUT',      // 在服务器更新资源（客户端提供改变后的完整资源）
+        'PATCH',    // 在服务器更新资源（客户端提供改变的属性）
+        'DELETE',   // 从服务器删除资源
+        'HEAD',     // 获取 head 元数据
+        'OPTIONS',  // 获取信息，关于资源的哪些属性是客户端可以改变的
+     */
     function index()
     {
         // TODO: Implement index() method.
