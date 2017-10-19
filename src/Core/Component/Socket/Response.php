97e86ff5860e0315b4863937a5717c14b6a59d9b
kiss291323003 <291323003@qq.com>
2017-10-19 16:23:50 +0800

    update

--- src/Core/Component/Socket/Response.php
+++ src/Core/Component/Socket/Response.php
@@ -16,15 +16,15 @@ use Core\Swoole\Server;
 
 class Response
 {
-    static function response(Client $client,$data){
+    static function response(Client $client,$data,$eof = ''){
         if($client instanceof TcpClient){
             if($client->getClientType() == Type::WEB_SOCKET){
                 return Server::getInstance()->getServer()->push($client->getFd(),$data);
             }else{
-                return Server::getInstance()->getServer()->send($client->getFd(),$data,$client->getReactorId());
+                return Server::getInstance()->getServer()->send($client->getFd(),$data.$eof,$client->getReactorId());
             }
         }else if($client instanceof UdpClient){
-             return Server::getInstance()->getServer()->sendto($client->getAddress(),$client->getPort(),$data);
+             return Server::getInstance()->getServer()->sendto($client->getAddress(),$client->getPort(),$data.$eof);
         }else{
             trigger_error( "client is not validate");
             return false;
