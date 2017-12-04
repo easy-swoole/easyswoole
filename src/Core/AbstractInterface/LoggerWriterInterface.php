b3d189992f4ac3b3b4a84256994b5032bcaf28a3
YF <291323003@qq.com>
2017-12-04 17:46:46 +0800

    update

---
+++ src/Core/AbstractInterface/LoggerWriterInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2017/12/4
+ * Time: 下午5:46
+ */
+
+namespace EasySwoole\Core\AbstractInterface;
+
+
+interface LoggerWriterInterface
+{
+    static function writeLog($obj,$logCategory,$timeStamp);
+}
