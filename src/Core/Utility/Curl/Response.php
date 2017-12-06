2ca8ac34fb78b013d0bb2222daeaf32d4e917ff2
YF <291323003@qq.com>
2017-12-06 18:47:24 +0800

    修复cookie为session时的bug

--- src/Core/Utility/Curl/Response.php
+++ src/Core/Utility/Curl/Response.php
@@ -29,8 +29,8 @@ class Response
         preg_match_all("/Set-Cookie:(.*)\n/U",$this->headerLine,$ret);
         if(!empty($ret[0])){
             foreach($ret[0] as $item) {
-                preg_match('/(Cookie: )(.*?)(;)/',$item,$ret);
-                $ret = explode('=',$ret[2]);
+                preg_match('/(Cookie: )(.*?)(\r\n)/',$item,$ret);
+                $ret = explode('=',trim($ret[2],';'));
                 $cookie = new Cookie();
                 $cookie->setName($ret[0]);
                 $cookie->setValue($ret[1]);
