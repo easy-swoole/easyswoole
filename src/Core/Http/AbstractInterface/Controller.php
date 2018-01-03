a025a4c73df98cc5d2efd166463e199b16a59ce3
YF <291323003@qq.com>
2018-01-03 17:07:44 +0800

    优化逻辑

--- src/Core/Http/AbstractInterface/Controller.php
+++ src/Core/Http/AbstractInterface/Controller.php
@@ -21,69 +21,60 @@ abstract class Controller
     private $request;
     private $response;
     private $actionName;
-    private static $forbidMethod = [
-        'getActionName','onRequest','actionNotFound','afterAction','request','response','__call','__hook','onException'
-    ];
 
-    abstract function index();
+    protected abstract function index();
 
-    abstract function actionNotFound($action = null):void;
+    protected abstract function actionNotFound($action = null):void;
 
-    abstract function afterAction($actionName):void;
+    protected abstract function afterAction($actionName):void;
 
-    abstract function onException(\Exception $exception,$actionName):void;
+    protected abstract function onException(\Exception $exception,$actionName):void;
 
-    public function onRequest($action):?bool
+    protected function onRequest($action):?bool
     {
         return true;
     }
 
-    public function getActionName():string
+    protected function getActionName():string
     {
         return $this->actionName;
     }
 
-    public function resetAction(string $action):void
+    protected function resetAction(string $action):void
     {
         $this->actionName = $action;
     }
 
-    public function __hook(string $actionName,Request $request,Response $response):void
+    protected function __hook(string $actionName,Request $request,Response $response):void
     {
         $this->request = $request;
         $this->response = $response;
         $this->actionName = $actionName;
-        //防止恶意调用
-        if(in_array($actionName,self::$forbidMethod)){
-            $response->withStatus(Status::CODE_INTERNAL_SERVER_ERROR);
-            $response->end();
-        }else{
-            if($this->onRequest($actionName) !== false){
-                $this->onRequest($actionName);
-                //防止onRequest中   对actionName 进行修改
-                $actionName = $this->actionName;
-                //支持在子类控制器中以private，protected来修饰某个方法不可见
-                $ref = new \ReflectionClass(static::class);
-                if($ref->hasMethod($actionName) && $ref->getMethod($actionName)->isPublic()){
-                    try{
-                        $this->$actionName();
-                    }catch (\Exception $exception){
-                        $this->onException($exception,$actionName);
-                    }
-                    $this->afterAction($actionName);
-                }else{
-                    $this->actionNotFound($actionName);
+        if($this->onRequest($actionName) !== false){
+            $this->onRequest($actionName);
+            //防止onRequest中   对actionName 进行修改
+            $actionName = $this->actionName;
+            //支持在子类控制器中以private，protected来修饰某个方法不可见
+            $ref = new \ReflectionClass(static::class);
+            if($ref->hasMethod($actionName) && $ref->getMethod($actionName)->isPublic()){
+                try{
+                    $this->$actionName();
+                }catch (\Exception $exception){
+                    $this->onException($exception,$actionName);
                 }
+                $this->afterAction($actionName);
+            }else{
+                $this->actionNotFound($actionName);
             }
         }
     }
 
-    final public function request():Request
+    final protected function request():Request
     {
         return $this->request;
     }
 
-    final public function response():Response
+    final protected function response():Response
     {
         return $this->response;
     }
@@ -91,7 +82,7 @@ abstract class Controller
     /*
      * 若不想用自带验证器，可以自己新建base控制器，重写validateParams方法
      */
-    public function validateParams(Rules $rules)
+    protected function validateParams(Rules $rules)
     {
         $validate = new Validate();
         return $validate->validate($this->request()->getRequestParam(),$rules);
