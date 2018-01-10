31c955cebf78f4eb07ac1096d6d6a8954fa962e2
YF <291323003@qq.com>
2018-01-10 16:30:59 +0800

    新增自动化测试

---
+++ test/SplStringTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2018/1/10
+ * Time: 下午4:25
+ */
+
+namespace EasySwoole\Test;
+
+
+use PHPUnit\Framework\TestCase;
+
+class SplStringTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+
+    }
+}
