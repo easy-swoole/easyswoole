668a953c5c5f740539a0b575675ee986dba3bbdf
如果的如果 <291323003@qq.com>
2018-01-30 12:25:12 +0800

    fix bug

--- src/Core/Http/AbstractInterface/Controller.php
+++ src/Core/Http/AbstractInterface/Controller.php
@@ -79,7 +79,6 @@ abstract class Controller
                 }catch (\Exception $exception){
                     $this->onException($exception,$actionName);
                 }
-                $this->afterAction($actionName);
             }else{
                 $this->actionNotFound($actionName);
             }
