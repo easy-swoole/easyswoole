734e535955c4959bcfec3da8d2fffbebfa0cb373
如果的如果 <291323003@qq.com>
2018-01-31 15:41:55 +0800

    update

---
+++ src/Core/Component/Rpc/Common/Signature.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2018/1/31
+ * Time: 下午3:22
+ */
+
+namespace EasySwoole\Core\Component\Rpc\Common;
+
+
+class Signature
+{
+    static function signature()
+    {
+
+    }
+
+    static function check()
+    {
+
+    }
+}
