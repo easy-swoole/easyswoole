27eb694e405dd881f5c2c7ef762804fe35c614ce
如果的如果 <291323003@qq.com>
2018-03-08 17:24:39 +0800

    合理性变更

--- src/Core/AbstractInterface/Singleton.php
+++ src/Core/AbstractInterface/Singleton.php
@@ -7,54 +7,16 @@
  */
 
 namespace EasySwoole\Core\AbstractInterface;
-use EasySwoole\Core\Swoole\ServerManager;
 
 trait Singleton
 {
-    private static $instanceList = [];
+    private static $instance;
 
-    static function getInstance($saveInCoroutine = false,...$args)
+    static function getInstance(...$args)
     {
-        if($saveInCoroutine == false){
-            $cid = 0;
-        }else{
-            $cid = self::getInstanceId();
+        if(!isset(self::$instance)){
+            self::$instance = new static(...$args);
         }
-        if(!isset(self::$instanceList[$cid])){
-            $ins = new static(...$args);
-            self::$instanceList[$cid] = $ins;
-        }else{
-            /*
-            * 为了IDE提示才
-            */
-            $ins = self::$instanceList[$cid];
-        }
-        return $ins;
-    }
-
-    static function getInstanceId():int
-    {
-        $cid = ServerManager::getInstance()->coroutineId();
-        if($cid === null){
-            $cid = 0;
-        }
-        return $cid;
-    }
-
-    /*
-     * 在携程模式下，任何以携程安全模式创建的单例  都应该被释放  否则内存泄漏
-     */
-
-    public function freeInstance($instanceId = null):?bool
-    {
-        if($instanceId === null){
-            $instanceId = self::getInstanceId();
-        }
-        if(isset(self::$instanceList[$instanceId])){
-            unset(self::$instanceList[$instanceId]);
-            return true;
-        }else{
-            return false;
-        }
+        return self::$instance;
     }
 }
