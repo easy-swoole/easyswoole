ec4d1a43dc68b7e28cadae6f54687cd381765f6a
windrunner414 <windrunner414@outlook.com>
2018-03-22 12:19:15 +0800

    update

--- src/Core/Component/Cluster/Common/EventRegister.php
+++ src/Core/Component/Cluster/Common/EventRegister.php
@@ -10,6 +10,7 @@ namespace EasySwoole\Core\Component\Cluster\Common;
 
 use EasySwoole\Core\AbstractInterface\Singleton;
 use EasySwoole\Core\Component\Cluster\Communicate\CommandBean;
+use EasySwoole\Core\Component\Cluster\Communicate\Publisher;
 use EasySwoole\Core\Component\Event;
 
 
@@ -28,8 +29,8 @@ class EventRegister extends Event
         $this->set(self::CLUSTER_ON_COMMAND,function (CommandBean $commandBean,...$args){
             CommandRegister::getInstance()->hook($commandBean->getCommand(),$commandBean,...$args);
         });
-        $this->set(self::CLUSTER_SHUTDOWN, function(){
-            
+        $this->set(self::CLUSTER_SHUTDOWN, function (CommandBean $commandBean){
+            Publisher::broadcast($commandBean);
         });
     }
 }
