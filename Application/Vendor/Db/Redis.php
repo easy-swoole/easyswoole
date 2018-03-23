86f0acd28e70d52701d050ae197d270486ae05de
azerothYang <azerothyangg@gmail.com>
2018-03-24 00:42:37 +0800

    redis add disconnect-reconnect

--- Application/Vendor/Db/Redis.php
+++ Application/Vendor/Db/Redis.php
@@ -15,6 +15,12 @@ class Redis
 {
     private static $instance;
 
+    //尝试连接当前的次数
+    protected $tryConnectTimes = 0;
+
+    //最大尝试次数
+    protected $maxTryConnectTimes = 3;
+
     /**
      * @var \Redis
      */
@@ -27,13 +33,24 @@ class Redis
         return self::$instance;
     }
 
-    function __construct()
-    {
+    function __construct(){
+        $this->connect();
+    }
+
+    function connect(){
         $conf = Config::getInstance()->getConf("REDIS");
         $this->con = new \Redis();
         $this->con->connect($conf['host'], $conf['port'], 3);
         $this->con->auth($conf['auth']);
         $this->con->select($conf['db']);
+        if(!$this->con->ping()){
+            if($this->tryConnectTimes <= $this->maxTryConnectTimes){
+                return $this->connect();
+            }else{
+                trigger_error("redis connect fail");
+                return null;
+            }
+        }
 //        $this->con->setOption(\Redis::OPT_SERIALIZER,\Redis::SERIALIZER_PHP); 暂时不开启php序列化
     }
 
@@ -41,6 +58,11 @@ class Redis
      * @return \Redis
      */
     function getConnect(){
-        return $this->con;
+        //ping 如果能ping通直接连接，ping不通则尝试重连
+        if($this->con->ping()){
+            return $this->con;
+        }
+        $this->tryConnectTimes = 0; //当前重连次数归零, 如果没有归零则会导致redis恢复后不会重连了
+        $this->connect();
     }
 }
