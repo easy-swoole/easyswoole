9b3a2d089d2d9d80297d3feafd3c8d0e097f896b
azerothYang <azerothyangg@gmail.com>
2018-04-09 14:52:41 +0800

    comment

--- Application/ExceptionHandler.php
+++ Application/ExceptionHandler.php
@@ -14,7 +14,7 @@ use EasySwoole\Core\Http\Request;
 use EasySwoole\Core\Http\Response;
 
 /**
- * 统一异常处理, 包括想屏蔽异常和记录异常
+ * 统一异常处理, 包括想屏蔽异常和记录异常, 这里在生产环境需要屏蔽所有异常, 同时将异常写入日志
  * Class ExceptionHandler
  * @package App
  */
