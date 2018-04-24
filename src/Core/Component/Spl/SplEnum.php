1d761a44755141f2c30a21169c7e1bbf37e40769
如果的如果 <291323003@qq.com>
2018-04-24 21:13:39 +0800

    完善SplEnum

--- src/Core/Component/Spl/SplEnum.php
+++ src/Core/Component/Spl/SplEnum.php
@@ -18,14 +18,15 @@ class SplEnum
 
     final public function __construct($val = null)
     {
-        $list = $this->getConstants();
+        $list = self::getConstants();
+        //禁止重复值
         if (count($list) != count(array_unique($list))) {
             $class = static::class;
             throw new \Exception("class : {$class} define duplicate value");
         }
         if($val !== null){
             $this->val = $val;
-            $this->name = array_search($val,$list);
+            $this->name = self::isValidValue($val);
             if($this->name === false){
                 throw new \Exception("invalid value");
             }
@@ -42,12 +43,34 @@ class SplEnum
         return $this->val;
     }
 
-    private final function getConstants():array
+    final public static function isValidName(string $name):bool
+    {
+        $list = self::getConstants();
+        if(isset($list[$name])){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    final public static function isValidValue($val)
+    {
+        $list = self::getConstants();
+        return array_search($val,$list);
+    }
+
+    private final static function getConstants():array
     {
         try{
             return (new \ReflectionClass(static::class))->getConstants();
         }catch (\Throwable $throwable){
             return [];
         }
+    }
+
+    final function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return (string)$this->val;
     }
 }
