789e2fad50e689518ef15fa4e0b63261dc9fb4ce
yf <291323003@qq.com>
2018-05-05 15:20:27 +0800

    把验证规则移动到Rule类

--- src/Core/Utility/Validate/Rule.php
+++ src/Core/Utility/Validate/Rule.php
@@ -12,6 +12,35 @@ namespace EasySwoole\Core\Utility\Validate;
 class Rule
 {
     private $rules = [];
+
+    const ACTIVE_URL = 'ACTIVE_URL';
+    const ALPHA = 'ALPHA';
+    const BETWEEN = 'BETWEEN';
+    const BOOLEAN = 'BOOLEAN';
+    const DIFFERENT = 'DIFFERENT';
+    const DATE = 'DATE';
+    const DATE_AFTER = 'DATE_AFTER';
+    const DATE_BEFORE = 'DATE_BEFORE';
+    const EMPTY = 'EMPTY';
+    const FLOAT = 'FLOAT';
+    const IN = 'IN';
+    const INTEGER = 'INTEGER';
+    const IP = 'IP';
+    const IS_ARRAY = 'IS_ARRAY';
+    const LEN = 'LEN';
+    const NOT_IN = 'NOT_IN';
+    const NUMERIC = 'NUMERIC';
+    const MAX = 'MAX';
+    const MAX_LEN = 'MAX_LEN';
+    const MIN = 'MIN';
+    const MIN_LEN = 'MIN_LEN';
+    const OPTIONAL = 'OPTIONAL';
+    const REGEX = 'REGEX';
+    const REQUIRED = 'REQUIRED';
+    const SAME = 'SAME';
+    const TIMESTAMP = 'TIMESTAMP';
+    const URL = 'URL';
+
     public function withRule(string $rule,...$args):Rule
     {
         $this->rules[$rule] = $args;
--- src/Core/Utility/Validate/Validate.php
+++ src/Core/Utility/Validate/Validate.php
@@ -13,34 +13,6 @@ use EasySwoole\Core\Component\Spl\SplArray;
 
 class Validate
 {
-    const ACTIVE_URL = 'ACTIVE_URL';
-    const ALPHA = 'ALPHA';
-    const BETWEEN = 'BETWEEN';
-    const BOOLEAN = 'BOOLEAN';
-    const DIFFERENT = 'DIFFERENT';
-    const DATE = 'DATE';
-    const DATE_AFTER = 'DATE_AFTER';
-    const DATE_BEFORE = 'DATE_BEFORE';
-    const EMPTY = 'EMPTY';
-    const FLOAT = 'FLOAT';
-    const IN = 'IN';
-    const INTEGER = 'INTEGER';
-    const IP = 'IP';
-    const IS_ARRAY = 'IS_ARRAY';
-    const LEN = 'LEN';
-    const NOT_IN = 'NOT_IN';
-    const NUMERIC = 'NUMERIC';
-    const MAX = 'MAX';
-    const MAX_LEN = 'MAX_LEN';
-    const MIN = 'MIN';
-    const MIN_LEN = 'MIN_LEN';
-    const OPTIONAL = 'OPTIONAL';
-    const REGEX = 'REGEX';
-    const REQUIRED = 'REQUIRED';
-    const SAME = 'SAME';
-    const TIMESTAMP = 'TIMESTAMP';
-    const URL = 'URL';
-
     public function validate(array $data, Rules $rules): Result
     {
         // TODO: Implement validate() method.
@@ -52,7 +24,7 @@ class Validate
             $errorMsg = $item['errorMsg'];
             $filedRules = $item['rule'];
             $filedRules = $filedRules->getRules();
-            if(!isset($data[$filed]) && in_array(self::OPTIONAL,$filedRules)){
+            if(!isset($data[$filed]) && in_array(Rule::OPTIONAL,$filedRules)){
                 continue;
             }else{
                 foreach ($filedRules as $rule => $args){
