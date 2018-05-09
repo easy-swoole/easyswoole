f2c706c54dd70a238349d4f3c471439da86d15d3
windrunner414 <windrunner414@outlook.com>
2018-05-09 23:12:34 +0800

    i18n support

---
+++ EasySwooleEvent.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2018/1/9
+ * Time: 下午1:04
+ */
+
+namespace EasySwoole;
+
+use \EasySwoole\Core\AbstractInterface\EventInterface;
+use \EasySwoole\Core\Swoole\ServerManager;
+use \EasySwoole\Core\Swoole\EventRegister;
+use \EasySwoole\Core\Http\Request;
+use \EasySwoole\Core\Http\Response;
+
+Class EasySwooleEvent implements EventInterface {
+
+    public static function frameInitialize(): void
+    {
+        // TODO: Implement frameInitialize() method.
+        date_default_timezone_set('Asia/Shanghai');
+    }
+
+    public static function mainServerCreate(ServerManager $server,EventRegister $register): void
+    {
+        // TODO: Implement mainServerCreate() method.
+    }
+
+    public static function onRequest(Request $request,Response $response): void
+    {
+        // TODO: Implement onRequest() method.
+    }
+
+    public static function afterAction(Request $request,Response $response): void
+    {
+        // TODO: Implement afterAction() method.
+    }
+}
---
+++ src/Core/Component/I18n/I18n.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: windrunner414
+ * Date: 18-5-9
+ * Time: 下午9:59
+ */
+
+namespace EasySwoole\Core\Component\I18n;
+
+use EasySwoole\Config;
+use EasySwoole\Core\Component\Spl\SplArray;
+use EasySwoole\Core\AbstractInterface\Singleton;
+use EasySwoole\Core\Component\Trigger;
+
+class I18n
+{
+    use Singleton;
+    private $languageDir,
+        $defaultCategory,
+        $defaultLanguage,
+        $dict;
+
+    public function __construct()
+    {
+        $config = Config::getInstance()->getConf('I18N');
+        $this->languageDir = $config['languageDir'];
+        $this->defaultCategory = $config['defaultCategory'];
+        $this->defaultLanguage = $config['defaultLanguage'];
+        $this->dict = new SplArray();
+    }
+
+    public function loadLanguage(string $language, string $category, string $key)
+    {
+        $file = $this->languageDir . '/' . $language . '/' . $category . '.php';
+        if (!is_readable($file)) {
+            Trigger::throwable(new \Exception('The Language file: ' . $file . ' is not readable'));
+            return null;
+        }
+        $pack = require $file;
+        $this->dict->set($language . '.' . $category, $pack);
+        $text = $pack[$key] ?? null;
+        return $text;
+    }
+
+    public function translate(string $path, ?array $replace = null, ?string $language = null)
+    {
+        if (is_null($language)) {
+            $language = $this->defaultLanguage;
+        }
+        $tPath = explode('.', $path);
+        switch (count($tPath) <=> 2) {
+            case -1:
+                $category = $this->defaultCategory;
+                $key = $tPath[0];
+                break;
+            case 0:
+                $category = $tPath[0];
+                $key = $tPath[1];
+                break;
+            default:
+                Trigger::throwable(new \Exception('The format of path is incorrect'));
+                return null;
+        }
+        $path = $language . '.' . $category . '.' . $key;
+        $text = $this->dict->get($path) ?? $this->loadLanguage($language, $category, $key);
+        if (is_null($text)) {
+            Trigger::throwable(new \Exception($path . ' not found'));
+            return null;
+        }
+        if (empty($replace)) {
+            return $text;
+        } else {
+            return sprintf($text, ...$replace);
+        }
+    }
+}
