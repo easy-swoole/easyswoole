0e7665dd8ea7ceaf4603c1ef0179940bc9c9759e
evalor <mipone@foxmail.com>
2018-05-12 12:15:11 +0800

    计划任务修改为按照Linux规则 (#89)

--- src/Core/Component/Crontab/Runner.php
+++ src/Core/Component/Crontab/Runner.php
@@ -23,31 +23,34 @@ class Runner extends AbstractProcess
         // TODO: Implement run() method.
         $this->allTask = $this->getArgs();
         //crontab最小执行单位是1min,因此29s检测一次,以半分钟为周期
-        $this->addTick(30000,function (){
+        $checkTask = function (){
             $allToDo = [];
             $current = time();
             foreach ($this->allTask as $task){
                 //若相差小于30s  则投递执行
-                try{
+                try {
                     $rule = $task['rule'];
                     $time = CronExpression::factory($rule)->getNextRunDate()->getTimestamp();
                     //过期任务不执行
-                    if($time > $current && ($time - $current <= 30)){
-                        $allToDo[] =  $task['task'];
-
+                    if ($time > $current && ($time - $current <= 30)) {
+                        $allToDo[] = ['task' => $task['task'], 'time' => $time - $current];
                     }
-                }catch (\Throwable $throwable){
+                } catch (\Throwable $throwable) {
                     Trigger::throwable($throwable);
                 }
             }
             if(!empty($allToDo)){
-                $this->delay(30000,function ()use($allToDo){
-                    foreach ($allToDo as $task){
-                        TaskManager::processAsync($task);
-                    }
-                });
+                // 检测时已得知距离目标执行时间的秒数
+                foreach ($allToDo as $task){
+                    $this->delay($task['time']*1000,function ()use($task){
+                        TaskManager::processAsync($task['task']);
+                    });
+                }
             }
-        });
+        };
+        // 因为定时器不是立马调用 启动时立即检查一次
+        $checkTask();
+        $this->addTick(30000,$checkTask);
     }
 
     public function onShutDown()
