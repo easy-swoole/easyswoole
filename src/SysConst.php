223d592eb49969c98af174d6df2ff0325b26d60f
yf <291323003@qq.com>
2018-08-14 00:23:53 +0800

    up

--- src/SysConst.php
+++ src/SysConst.php
@@ -11,7 +11,7 @@ namespace EasySwoole\EasySwoole;
 
 class SysConst
 {
-    const VERSION = '3.0.1';
+    const VERSION = '3.0.1-RC4';
     const ERROR_HANDLER = 'ERROR_HANDLER';
     const SHUTDOWN_FUNCTION = 'SHUTDOWN_FUNCTION';
 
