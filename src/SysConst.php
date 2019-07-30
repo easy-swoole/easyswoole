9ea84fa440067ee2ebc38db205d920a0ea3e1f59
yf <291323003@qq.com>
2019-07-30 11:25:32 +0800

    3.2.7-dev

--- src/SysConst.php
+++ src/SysConst.php
@@ -11,7 +11,7 @@ namespace EasySwoole\EasySwoole;
 
 class SysConst
 {
-    const EASYSWOOLE_VERSION = '3.2.6';
+    const EASYSWOOLE_VERSION = '3.2.7-dev';
     const LOGGER_HANDLER = 'LOGGER_HANDLER';
     const ERROR_HANDLER = 'ERROR_HANDLER';
     const TRIGGER_HANDLER = 'TRIGGER_HANDLER';
