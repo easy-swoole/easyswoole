ecf7003a3363307e9e8f42f2f6714be55a96799b
tioncico <1067197739@qq.com>
2019-09-05 16:50:56 +0800

    部分单元测试

---
+++ tests/EasySwooleEvent.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2018/5/28
+ * Time: 下午6:33
+ */
+
+namespace EasySwoole\EasySwoole;
+
+
+use EasySwoole\EasySwoole\Swoole\EventRegister;
+use EasySwoole\EasySwoole\AbstractInterface\Event;
+use EasySwoole\Http\Request;
+use EasySwoole\Http\Response;
+
+class EasySwooleEvent implements Event
+{
+
+    public static function initialize()
+    {
+        Config::getInstance()->setConf('SWOOLE_RUN',false);
+        // TODO: Implement initialize() method.
+        date_default_timezone_set('Asia/Shanghai');
+    }
+
+    public static function mainServerCreate(EventRegister $register)
+    {
+        Config::getInstance()->setConf('SWOOLE_RUN',true);
+        // TODO: Implement mainServerCreate() method.
+    }
+
+    public static function onRequest(Request $request, Response $response): bool
+    {
+        // TODO: Implement onRequest() method.
+        return true;
+    }
+
+    public static function afterRequest(Request $request, Response $response): void
+    {
+        // TODO: Implement afterAction() method.
+    }
+}
---
+++ tests/produce.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2019-01-01
+ * Time: 20:06
+ */
+
+return [
+    'SERVER_NAME' => "EasySwoole",
+    'MAIN_SERVER' => [
+        'LISTEN_ADDRESS' => '0.0.0.0',
+        'PORT' => 9501,
+        'SERVER_TYPE' => EASYSWOOLE_WEB_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER,EASYSWOOLE_REDIS_SERVER
+        'SOCK_TYPE' => SWOOLE_TCP,
+        'RUN_MODEL' => SWOOLE_PROCESS,
+        'SETTING' => [
+            'worker_num' => 8,
+            'reload_async' => true,
+            'max_wait_time'=>3
+        ],
+        'TASK'=>[
+            'workerNum'=>4,
+            'maxRunningNum'=>128,
+            'timeout'=>15
+        ]
+    ],
+    'TEMP_DIR' => null,
+    'LOG_DIR' => null,
+    'IS_DEV'=>false,
+];
