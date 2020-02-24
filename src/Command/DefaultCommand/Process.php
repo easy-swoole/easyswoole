75693c5a628894cb00feddb3fbbef4f8b510ea00
如果的如果 <admin@fosuss.com>
2020-02-24 15:08:03 +0800

    up

--- src/Command/DefaultCommand/Process.php
+++ src/Command/DefaultCommand/Process.php
@@ -24,6 +24,24 @@ class Process implements CommandInterface
          * php easyswoole process killAll
          * php easyswoole process killAll -f
          */
+        $file = EASYSWOOLE_TEMP_DIR.'/process.json';
+        if(file_exists($file)){
+            $action = array_shift($args);
+            $pidOrGroupName = array_shift($args);
+            $option = array_shift($args);
+            $json = json_decode(file_get_contents($file),true);
+            switch ($action){
+                case 'kill':{
+                    break;
+                }
+                case 'killAll':{
+                    break;
+                }
+            }
+            return  $this->help($args);
+        }else{
+            return "there is not process info";
+        }
     }
 
     public function help(array $args): ?string
