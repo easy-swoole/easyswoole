63779a0033cf88d49239aa1e0bdf25001e3cd05b
如果的如果 <admin@fosuss.com>
2020-02-24 14:22:33 +0800

    up

--- src/Process/BaseService.php
+++ src/Process/BaseService.php
@@ -9,15 +9,23 @@ use EasySwoole\Component\Timer;
 
 class BaseService extends AbstractProcess
 {
+    private $processJsonFile;
     protected function run($arg)
     {
         /*
          * 本进程用于做Easyswoole后续的一些基础附加服务
          */
-        $dir = EASYSWOOLE_TEMP_DIR.'/process.json';
-        Timer::getInstance()->loop(1*1000,function ()use($dir){
+        $this->processJsonFile = EASYSWOOLE_TEMP_DIR.'/process.json';
+        Timer::getInstance()->loop(1*1000,function (){
             $list = Manager::getInstance()->info();
-            file_put_contents($dir,json_encode($list,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE));
+            file_put_contents($this->processJsonFile,json_encode($list,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE));
         });
+    }
+
+    protected function onShutDown()
+    {
+        if(is_file($this->processJsonFile)){
+            unlink($this->processJsonFile);
+        }
     }
 }
