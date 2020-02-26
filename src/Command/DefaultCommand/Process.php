4354e2d0b624293b95fbc7dcf75b758885e2dc97
如果的如果 <admin@fosuss.com>
2020-02-26 11:26:05 +0800

    up

--- src/Command/DefaultCommand/Process.php
+++ src/Command/DefaultCommand/Process.php
@@ -27,18 +27,49 @@ class Process implements CommandInterface
         $file = EASYSWOOLE_TEMP_DIR.'/process.json';
         if(file_exists($file)){
             $action = array_shift($args);
-            $pidOrGroupName = array_shift($args);
-            $option = array_shift($args);
             $json = json_decode(file_get_contents($file),true);
+            $list = [];
             switch ($action){
                 case 'kill':{
+                    $pidOrGroupName = array_shift($args);
+                    $option = array_shift($args);
+                    if(is_numeric($pidOrGroupName)){
+                        $list[] = $pidOrGroupName;
+                    }else{
+                        foreach ($json as $pid => $value){
+                            if($value['group'] == $pidOrGroupName){
+                                $list[] = $pid;
+                            }
+                        }
+                    }
                     break;
                 }
                 case 'killAll':{
+                    $option = array_shift($args);
+                    foreach ($json as $pid => $value){
+                        $list[] = $pid;
+                    }
                     break;
                 }
+                default:{
+                    return  $this->help($args);
+                }
             }
-            return  $this->help($args);
+            if(empty($list)){
+                return 'not process was kill';
+            }else{
+                if($option == '-f'){
+                    $sig = 9;
+                }else{
+                    $sig = 15;
+                }
+                $ret = '';
+                foreach ($list as $pid){
+                    \Swoole\Process::kill($pid,$sig);
+                    $ret .= Utility::displayItem('kill pid :',$pid.' '.$option)."\n";
+                }
+                return $ret;
+            }
         }else{
             return "there is not process info";
         }
@@ -47,6 +78,12 @@ class Process implements CommandInterface
     public function help(array $args): ?string
     {
         $logo = Utility::easySwooleLog();
-        return $logo;
+        return $logo."
+php easyswoole process kill PID
+php easyswoole process kill PID -f
+php easyswoole process kill GroupName -f
+php easyswoole process killAll
+php easyswoole process killAll -f
+";
     }
 }
