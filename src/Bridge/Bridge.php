0bb5fe523f4f45381b22402d198fb1d48543258e
如果的如果 <admin@fosuss.com>
2020-04-06 00:05:11 +0800

    up fix

---
+++ src/Bridge/Bridge.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace EasySwoole\EasySwoole\Bridge;
+
+
+use EasySwoole\Component\Singleton;
+
+class Bridge
+{
+    use Singleton;
+}
---
+++ src/Bridge/ServiceProcess.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace EasySwoole\EasySwoole\Bridge;
+
+
+class ServiceProcess
+{
+
+}
