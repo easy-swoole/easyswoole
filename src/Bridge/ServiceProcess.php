05d0c1d0677dffb9b958a5c33fc8592310229f13
如果的如果 <admin@fosuss.com>
2020-04-06 21:13:52 +0800

    up fix

--- src/Bridge/ServiceProcess.php
+++ src/Bridge/ServiceProcess.php
@@ -6,13 +6,18 @@ namespace EasySwoole\EasySwoole\Bridge;
 
 use EasySwoole\Component\Process\Socket\AbstractUnixProcess;
 use EasySwoole\EasySwoole\Trigger;
+use EasySwoole\Socket\Tools\Protocol;
 use Swoole\Coroutine\Socket;
 
 class ServiceProcess extends AbstractUnixProcess
 {
     function onAccept(Socket $socket)
     {
-
+        $data = Protocol::socketReader($socket,3,false);
+        if($data){
+            $package = unserialize($data);
+        }
+        $socket->close();
     }
 
     protected function onException(\Throwable $throwable, ...$args)
