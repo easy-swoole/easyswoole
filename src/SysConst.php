72a999c49a4a4e8807010ed9be947db61cfd0e57
如果的如果 <admin@fosuss.com>
2020-04-13 17:42:36 +0800

    3.3.7-dev

--- src/SysConst.php
+++ src/SysConst.php
@@ -11,7 +11,7 @@ namespace EasySwoole\EasySwoole;
 
 class SysConst
 {
-    const EASYSWOOLE_VERSION = '3.3.6';
+    const EASYSWOOLE_VERSION = '3.3.7-dev';
     const LOGGER_HANDLER = 'LOGGER_HANDLER';
     const ERROR_HANDLER = 'ERROR_HANDLER';
     const TRIGGER_HANDLER = 'TRIGGER_HANDLER';
