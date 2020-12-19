829d848b28bf59377f57ad11a45bcc71244288a6
gaobinzhan <gaobinzhan@gmail.com>
2020-12-19 18:09:17 +0800

    remove: test

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -13,24 +13,9 @@ class CoreTest extends TestCase
         return parent::runTest();
     }
 
-    function testDev()
-    {
-        global $argv;
-        if(in_array('produce',$argv)){
-            $this->assertEquals(false,Core::getInstance()->isDev());
-        }else{
-            $this->assertEquals(true,Core::getInstance()->isDev());
-        }
-    }
-
     function testTempDir()
     {
         $this->assertEquals(EASYSWOOLE_ROOT.'/Temp',EASYSWOOLE_TEMP_DIR);
-    }
-
-    public function testSetIsDev() {
-        Core::getInstance()->setIsDev(true);
-        $this->assertTrue(Core::getInstance()->isDev());
     }
 
 }
