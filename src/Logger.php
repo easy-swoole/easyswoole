75118df52ea2f4f74af9cdadbdcf8e82a596d42a
如果的如果 <admin@fosuss.com>
2021-02-02 15:28:15 +0800

    Log Level support

--- src/Logger.php
+++ src/Logger.php
@@ -21,6 +21,8 @@ class Logger implements LoggerInterface
 
     private $logConsole = true;
 
+    private $logLevel = LoggerInterface::LOG_LEVEL_INFO;
+
     use Singleton;
 
     function __construct(LoggerInterface $logger)
@@ -29,46 +31,62 @@ class Logger implements LoggerInterface
         $this->callback = new Event();
     }
 
+    public function logLevel(?int $level = null):int
+    {
+        if($level !== null){
+            $this->logLevel = $level;
+        }
+        return $this->logLevel;
+    }
+
     public function setLogConsole(bool $is)
     {
         $this->logConsole = $is;
         return $this;
     }
 
-    public function log(?string $msg,int $logLevel = self::LOG_LEVEL_INFO,string $category = 'debug')
+    public function log(?string $msg,int $logLevel = self::LOG_LEVEL_DEBUG,string $category = 'debug')
     {
-        $this->logger->log($msg,$logLevel,$category);
+        if($logLevel >= $this->logLevel){
+            $this->logger->log($msg,$logLevel,$category);
+        }
         $calls = $this->callback->all();
         foreach ($calls as $call){
             call_user_func($call,$msg,$logLevel,$category);
         }
     }
 
-    public function console(?string $msg,int $logLevel = self::LOG_LEVEL_INFO,string $category = 'debug')
+    public function console(?string $msg,int $logLevel = self::LOG_LEVEL_DEBUG,string $category = 'debug')
     {
-        $this->logger->console($msg,$logLevel,$category);
+        if($logLevel >= $this->logLevel){
+            $this->logger->console($msg,$logLevel,$category);
+        }
         if($this->logConsole){
             $this->log($msg,$logLevel,$category);
         }
     }
 
+    public function debug(?string $msg,string $category = 'debug')
+    {
+        $this->console($msg,self::LOG_LEVEL_DEBUG,$category);
+    }
 
-    public function info(?string $msg,string $category = 'debug')
+    public function info(?string $msg,string $category = 'info')
     {
         $this->console($msg,self::LOG_LEVEL_INFO,$category);
     }
 
-    public function notice(?string $msg,string $category = 'debug')
+    public function notice(?string $msg,string $category = 'notice')
     {
         $this->console($msg,self::LOG_LEVEL_NOTICE,$category);
     }
 
-    public function waring(?string $msg,string $category = 'debug')
+    public function waring(?string $msg,string $category = 'waring')
     {
         $this->console($msg,self::LOG_LEVEL_WARNING,$category);
     }
 
-    public function error(?string $msg,string $category = 'debug')
+    public function error(?string $msg,string $category = 'error')
     {
         $this->console($msg,self::LOG_LEVEL_ERROR,$category);
     }
