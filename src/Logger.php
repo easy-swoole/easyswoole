b585cbe280f42d9794c09b56862d3e8d5711e0d9
gaobinzhan <gaobinzhan@gmail.com>
2021-03-07 10:53:55 +0800

    add: log ingore category

--- src/Logger.php
+++ src/Logger.php
@@ -35,7 +35,7 @@ class Logger
 
     public function logLevel(?int $level = null)
     {
-        if($level !== null){
+        if ($level !== null) {
             $this->logLevel = $level;
             return $this;
         }
@@ -44,9 +44,9 @@ class Logger
 
     public function logConsole(?bool $is = null)
     {
-        if($is === null){
+        if ($is === null) {
             return $this->logConsole;
-        }else{
+        } else {
             $this->logConsole = $is;
             return $this;
         }
@@ -54,63 +54,73 @@ class Logger
 
     public function ignoreCategory(?array $arr = null)
     {
-        if($arr === null){
+        if ($arr === null) {
             return $this->ignoreCategory;
-        }else{
+        } else {
             $this->ignoreCategory = $arr;
             return $this;
         }
     }
 
-    public function log(?string $msg,int $logLevel = LoggerInterface::LOG_LEVEL_DEBUG,string $category = 'debug')
+    public function log(?string $msg, int $logLevel = LoggerInterface::LOG_LEVEL_DEBUG, string $category = 'debug')
     {
-        if($logLevel < $this->logLevel){
+        if ($logLevel < $this->logLevel) {
             return;
         }
-        $this->logger->log($msg,$logLevel,$category);
+
+        if (in_array($category, $this->ignoreCategory)) {
+            return;
+        }
+
+        $this->logger->log($msg, $logLevel, $category);
         $calls = $this->callback->all();
-        foreach ($calls as $call){
-            call_user_func($call,$msg,$logLevel,$category);
+        foreach ($calls as $call) {
+            call_user_func($call, $msg, $logLevel, $category);
         }
     }
 
-    public function console(?string $msg,int $logLevel = LoggerInterface::LOG_LEVEL_DEBUG,string $category = 'debug')
+    public function console(?string $msg, int $logLevel = LoggerInterface::LOG_LEVEL_DEBUG, string $category = 'debug')
     {
-        if($logLevel < $this->logLevel){
+        if ($logLevel < $this->logLevel) {
             return;
         }
-        $this->logger->console($msg,$logLevel,$category);
-        if($this->logConsole){
-            $this->log($msg,$logLevel,$category);
+
+        if (in_array($category, $this->ignoreCategory)) {
+            return;
+        }
+
+        $this->logger->console($msg, $logLevel, $category);
+        if ($this->logConsole) {
+            $this->log($msg, $logLevel, $category);
         }
     }
 
-    public function debug(?string $msg,string $category = 'debug')
+    public function debug(?string $msg, string $category = 'debug')
     {
-        $this->console($msg,LoggerInterface::LOG_LEVEL_DEBUG,$category);
+        $this->console($msg, LoggerInterface::LOG_LEVEL_DEBUG, $category);
     }
 
-    public function info(?string $msg,string $category = 'info')
+    public function info(?string $msg, string $category = 'info')
     {
-        $this->console($msg,LoggerInterface::LOG_LEVEL_INFO,$category);
+        $this->console($msg, LoggerInterface::LOG_LEVEL_INFO, $category);
     }
 
-    public function notice(?string $msg,string $category = 'notice')
+    public function notice(?string $msg, string $category = 'notice')
     {
-        $this->console($msg,LoggerInterface::LOG_LEVEL_NOTICE,$category);
+        $this->console($msg, LoggerInterface::LOG_LEVEL_NOTICE, $category);
     }
 
-    public function waring(?string $msg,string $category = 'waring')
+    public function waring(?string $msg, string $category = 'waring')
     {
-        $this->console($msg,LoggerInterface::LOG_LEVEL_WARNING,$category);
+        $this->console($msg, LoggerInterface::LOG_LEVEL_WARNING, $category);
     }
 
-    public function error(?string $msg,string $category = 'error')
+    public function error(?string $msg, string $category = 'error')
     {
-        $this->console($msg,LoggerInterface::LOG_LEVEL_ERROR,$category);
+        $this->console($msg, LoggerInterface::LOG_LEVEL_ERROR, $category);
     }
 
-    public function onLog():Event
+    public function onLog(): Event
     {
         return $this->callback;
     }
