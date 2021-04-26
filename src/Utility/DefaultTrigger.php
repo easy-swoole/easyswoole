4c56d434337627fbd3aaeda845c8a0fc09251200
gaobinzhan <gaobinzhan@gmail.com>
2021-04-26 18:40:17 +0800

    Feat: DefaultTrigger

--- src/Utility/DefaultTrigger.php
+++ src/Utility/DefaultTrigger.php
@@ -4,6 +4,8 @@
 namespace EasySwoole\EasySwoole\Utility;
 
 
+use EasySwoole\EasySwoole\Logger;
+use EasySwoole\Log\LoggerInterface;
 use EasySwoole\Trigger\Location;
 use EasySwoole\Trigger\TriggerInterface;
 
@@ -12,11 +14,47 @@ class DefaultTrigger implements TriggerInterface
 
     public function error($msg, int $errorCode = E_USER_ERROR, Location $location = null)
     {
-        // TODO: Implement error() method.
+        if ($location == null) {
+            $location = new Location();
+            $debugTrace = debug_backtrace();
+            $caller = array_shift($debugTrace);
+            $location->setLine($caller['line']);
+            $location->setFile($caller['file']);
+        }
+
+        Logger::getInstance()->log("{$msg} at file:{$location->getFile()} line:{$location->getLine()}", $this->errorMapLogLevel($errorCode));
     }
 
     public function throwable(\Throwable $throwable)
     {
-        // TODO: Implement throwable() method.
+        $msg = "{$throwable->getMessage()} at file:{$throwable->getFile()} line:{$throwable->getLine()}";
+        Logger::getInstance()->log($msg, LoggerInterface::LOG_LEVEL_ERROR,'');
+    }
+
+    private function errorMapLogLevel(int $errorCode)
+    {
+        switch ($errorCode) {
+            case E_PARSE:
+            case E_ERROR:
+            case E_CORE_ERROR:
+            case E_COMPILE_ERROR:
+            case E_USER_ERROR:
+                return LoggerInterface::LOG_LEVEL_ERROR;
+            case E_WARNING:
+            case E_USER_WARNING:
+            case E_COMPILE_WARNING:
+            case E_RECOVERABLE_ERROR:
+                return LoggerInterface::LOG_LEVEL_WARNING;
+            case E_NOTICE:
+            case E_USER_NOTICE:
+                return LoggerInterface::LOG_LEVEL_NOTICE;
+            case E_STRICT:
+                return LoggerInterface::LOG_LEVEL_NOTICE;
+            case E_DEPRECATED:
+            case E_USER_DEPRECATED:
+                return LoggerInterface::LOG_LEVEL_NOTICE;
+            default :
+                return LoggerInterface::LOG_LEVEL_INFO;
+        }
     }
 }
