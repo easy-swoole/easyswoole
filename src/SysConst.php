2601518a8e0c488679269cad0d783d5f56e617ed
如果的如果 <admin@fosuss.com>
2023-02-22 20:41:52 +0800

    3.6.4-dev

--- src/SysConst.php
+++ src/SysConst.php
@@ -11,7 +11,7 @@ namespace EasySwoole\EasySwoole;
 
 class SysConst
 {
-    const EASYSWOOLE_VERSION = '3.6.3';
+    const EASYSWOOLE_VERSION = '3.6.4-dev';
 
     const LOGGER_HANDLER = 'LOGGER_HANDLER';
     const ERROR_HANDLER = 'ERROR_HANDLER';
